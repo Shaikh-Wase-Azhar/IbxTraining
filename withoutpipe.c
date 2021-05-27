4bc8c62e9aed3fd3414598717e51a82cf226dafc
Shaikh-Wase-Azhar <67830377+Shaikh-Wase-Azhar@users.noreply.github.com>
2021-05-27 22:16:27 +0530

    Add files via upload

---
+++ withoutpipe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_mutex_t lock;
+int arr[10];
+
+void *in(void *arg)
+{
+    for(int i=0; i<10; i++)
+        {pthread_mutex_lock(&lock);  //lock
+        printf("T1:Enter the integer:\n");
+        scanf("%d",arr[i]);
+
+        pthread_mutex_unlock(&lock);  //unlock
+    }
+    return NULL;
+}
+
+
+void *out(void *arg)
+{
+        for(int i=0; i<10; i++)
+        {
+        pthread_mutex_lock(&lock);  //lock
+        printf("T2:%dth integer is:%d\n",i,arr[i]);
+        pthread_mutex_unlock(&lock);  //unlock
+        }
+    return NULL;
+}
+
+int main ()
+{
+    pthread_t tid1,tid2;
+    pthread_mutex_init(&lock,NULL);
+    pthread_create(&tid1, NULL, &in, NULL);
+    pthread_create(&tid2, NULL, &out, NULL);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_mutex_destroy(&lock);
+    return 0;
+}
+
